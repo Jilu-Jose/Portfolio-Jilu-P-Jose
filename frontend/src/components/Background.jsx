edbabf6f083785d786e6f311d19fc55e523c9f50
Jilu P Jose <jilupjose111@gmail.com>
2026-03-18 09:51:49 +0530

    SEctions Updated

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -2,11 +2,20 @@ import './Background.css';
 
 const Background = () => {
     return (
-        <div className="animated-bg">
-            <div className="orb orb-1"></div>
-            <div className="orb orb-2"></div>
-            <div className="orb orb-3"></div>
-            <div className="grid-overlay"></div>
+        <div className="portfolio-bg">
+            <div className="bg-ambient-light light-red"></div>
+            <div className="bg-ambient-light dark-red"></div>
+            <div className="bg-ambient-light deep-grey"></div>
+            
+            <div className="bg-grid-tech"></div>
+            
+            <div className="bg-particles">
+                <div className="particle pr-1"></div>
+                <div className="particle pr-2"></div>
+                <div className="particle pr-3"></div>
+                <div className="particle pr-4"></div>
+                <div className="particle pr-5"></div>
+            </div>
         </div>
     );
 };
